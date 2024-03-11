0b37f493a7c89dedf7550c7236025c3364075cdd
npm_am <npm_am@Kubuntu.myguest.virtualbox.org>
2024-03-11 19:19:44 +0200

    autentificare

---
+++ Autentificare/autentificare.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autentificare</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/Autentificare/autentificare.css"> <!-- Link your custom CSS here -->
+</head>
+<body>
+    <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-lg-7 col-md-7 col-sm-6 p-0">
+                <img src="/Imagini/RouteRoverCover.jpg" class="img-fluid img-full-height" alt="Route Rover Cover">
+            </div>
+            <div class="col-lg-5 col-md-5 col-sm-6 d-flex">
+                <div class="full-height-form w-100">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Nume de utilizator">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Parola">
+                        </div>
+                        <div class="d-flex flex-column">
+                            <button type="submit" class="btn btn-primary mb-3">Conectare</button>
+                            <span class="text-center">---SAU---</span>
+                            <button type="button" class="btn btn-primary mt-3">Înscrie-te</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
