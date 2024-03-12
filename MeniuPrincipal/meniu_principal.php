38f275a533999bdb894473f6bbad4413af8d9ecf
npm_am <npm_am@Kubuntu.myguest.virtualbox.org>
2024-03-12 22:46:59 +0200

    am adaugat meniul principal

--- MeniuPrincipal/meniu_principal.php
+++ MeniuPrincipal/meniu_principal.php
@@ -4,8 +4,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meniu Principal</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    <link href="/MeniuPrincipal/meniu_principal.css" rel="stylesheet">
+    <style>
+        .navbar-logo {
+            max-height: 50px;
+        }
+
+        .center-nav {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+
+        @media (max-width: 992px) { /* Ajustează breakpoint-ul conform design-ului Bootstrap */
+            .center-nav {
+                position: static;
+                transform: none;
+            }
+        }
+    </style>
 </head>
 <body>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam rem debitis itaque alias nesciunt quas necessitatibus dignissimos? Id assumenda eligendi provident cupiditate labore non unde mollitia, minus distinctio cumque reiciendis?</p>
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="/MeniuPrincipal/meniu_principal.php">
+            <img src="/Imagini/Logo.png" class="navbar-logo d-inline-block align-top" alt="Logo">
+            Route Rover
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto center-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/MeniuPrincipal/meniu_principal.php"><i class="fas fa-home"></i> Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-user"></i> Profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-car"></i> Șoferi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-file-alt"></i> Documente</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-file-contract"></i> Contracte</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-truck"></i> Vehicule</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-tasks"></i> Task nou</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Conținutul principal al paginii -->
+    <div class="container">
+        <h1 class="text-center">Welcome to Route Rover!</h1>
+        <!-- Restul conținutului paginii... -->
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
